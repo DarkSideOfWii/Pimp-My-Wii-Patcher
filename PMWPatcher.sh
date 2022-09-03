19bd8677888e4e10d4f69fb7e82616be6a033343
Joshua Faganello <112542668+DarkSideOfWii@users.noreply.github.com>
2022-09-03 10:44:00 +0200

    Add files via upload

---
+++ PMWPatcher.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cd Desktop/
+mkdir Pimp My Wii && cd Pimp My Wii
+git clone https://github.com/DarkSideOfWii/Pimp-My-Wii-Patcher.git
+echo " "
+echo "Thanks to used the patcher, good luck"
